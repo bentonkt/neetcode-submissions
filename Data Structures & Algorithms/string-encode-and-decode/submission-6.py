a148fc1633176115e14e54fd7b47861f05360b9b
Benton Tameling <86175015+bentonkt@users.noreply.github.com>
2026-09-05 12:06:44 -0700

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        lens = [str(len(s)) + "|" + s for s in strs]
+        return "".join(lens)
+
+
+    def decode(self, s: str) -> List[str]:
+        strs = []
+        i = 0
+        while i < len(s): 
+            # Get num
+            num = 0
+            while s[i] != "|":
+                num = num * 10 + int(s[i])
+                i += 1
+            strs.append(s[i+1:i+1+num])
+            i += num + 1
+
+        return strs
