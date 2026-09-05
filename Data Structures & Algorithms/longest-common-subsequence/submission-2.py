c01933e1b5eadf48177e7614c4cccd405a1e5a7a
Benton Tameling <86175015+bentonkt@users.noreply.github.com>
2026-09-05 11:19:59 -0700

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.py
@@ -0,0 +1,29 @@
+import numpy as np 
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = list(np.zeros((len(text1), len(text2)), dtype=int))
+        # mxn
+        m, n = len(text1), len(text2)
+        for row in range(m-1, -1, -1):
+            for col in range(n-1, -1, -1):
+                c1 = text1[row]
+                c2 = text2[col]
+
+                base = 0
+                if col + 1 < n and memo[row][col+1] > base:
+                    base = memo[row][col+1]
+                if row + 1 < m and memo[row+1][col] > base:
+                    base = memo[row+1][col]
+
+                if c1 == c2: 
+                    if row +1 < m and col + 1 < n:
+                        base = 1 + memo[row+1][col+1]
+                    else:
+                        base = 1
+
+
+
+
+                memo[row][col] = base
+
+        return int(memo[0][0])
