64f54eedd0b96aac5e551ef9f8ef0c94c67afe1c
Benton Tameling <86175015+bentonkt@users.noreply.github.com>
2026-09-09 19:41:34 -0700

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = set()
+        memo = defaultdict(bool)
+
+        def dfs(i, a): 
+            if i >= len(nums):
+                return
+
+            nonlocal res
+
+
+            num = nums[i]
+
+            soFar = sum(a)
+            # if memo[(soFar, i)]:
+            #     return
+            if soFar > target: 
+                return
+
+            if soFar + num == target:
+                a.append(num)
+                res.add(tuple(a))
+                memo[(soFar, i)] = True
+                a.pop()
+
+            dfs(i+1, a.copy())
+
+            if soFar + num > target:
+                return
+
+            a.append(num)
+            dfs(i, a.copy())
+            
+
+        dfs(0, [])
+
+        return [list(c) for c in res]
