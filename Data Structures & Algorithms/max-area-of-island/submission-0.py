50706bffabf818b339d88db64e45f3cc39b060f1
Benton Tameling <86175015+bentonkt@users.noreply.github.com>
2026-09-13 11:24:33 -0400

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[str]]) -> int:
+        directions = [[0, 1], [1,0], [0, -1], [-1, 0]]
+
+        explored = set()
+        res = 0
+        islands = 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+            
+                if (row, col) in explored or grid[row][col] == 0:
+                    continue
+                queue = deque()
+                queue.append((row, col))
+                # print("_______")
+                size = 0
+                while queue: 
+                    # print(queue)
+                    y, x = queue.popleft()
+
+                    if (y, x) in explored:
+                        continue
+
+                    explored.add((y, x))
+                    size += 1
+
+                    if grid[y][x]:
+                        for d1, d2 in directions:
+                            if 0 <= y+d1 and y+d1 < len(grid) and 0 <= x+d2 and x+d2 < len(grid[0]) and grid[y+d1][x+d2] == 1:
+                                if (y+d1, x+d2) not in explored:
+                                    queue.append((y+d1, x+d2))
+
+                res = max(res, size)
+
+        return res
