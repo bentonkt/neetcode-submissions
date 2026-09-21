ef7ae9fadaa676ebbbd6c9f114e637951857d30d
Benton Tameling <86175015+bentonkt@users.noreply.github.com>
2026-09-21 14:41:09 -0700

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        threshold = len(nums) / 3
+
+        counts = defaultdict(int)
+        res = set()
+
+        for num in nums:
+            counts[num] += 1
+            
+            if counts[num] > threshold:      
+                res.add(num)
+
+
+        return list(res)
